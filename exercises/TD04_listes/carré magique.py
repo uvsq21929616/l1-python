a77c28c3dc9cf395deccad32ddc535da3a297907
Elise Reber <elise.reber@ens.uvsq.fr>
2020-12-18 19:34:27 +0100

    igio

--- exercises/TD04_listes/carré magique.py
+++ exercises/TD04_listes/carré magique.py
@@ -7,12 +7,35 @@ def afficheCarre(carre):
         print(carre[i])
 
 
-afficheCarre(carre_mag)
-afficheCarre(carre_pas_mag)
-
-
 def testLignesEgales(carre):
+    """ Renvoie la somme des éléments d'une ligne de la liste 2D carre si 
+        toutes les lignes ont la même somme, et -1 sinon """
+    somme = []
     for i in range(len(carre)):
         a = 0
         for j in range(len(carre[i])):
             a = a + carre[i][j]
+        somme.append(a)
+    if somme[0] == somme[2] == somme[3] == somme[1]:
+        print(somme[1])
+    else:
+        print(-1)
+
+
+def testColonnesEgales(carre):
+    """ Renvoie la somme des éléments d'une colonne de la liste 2D carre si 
+        toutes les colonnes ont la même somme, et -1 sinon """
+    somme = []
+    for j in range(len(carre[1])):
+        a = 0
+        for i in range(len(carre)):
+            a = a + carre[i][j]
+        somme.append(a)
+    if somme[0] == somme[1] == somme[2] == somme[3]:
+        print(somme[0])
+    else:
+        print(-1)
+    return
+
+
+print(testLignesEgales(carre_pas_mag))
